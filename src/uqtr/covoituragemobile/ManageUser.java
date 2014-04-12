e88c843f56241c6ca1b8c64328be6efcf63c4304
VincentHould <vincenthould39@hotmail.com>
2014-04-12 15:37:52 -0400

    faire le layout pour la fiche du user + quick fix dans les ads

---
+++ src/uqtr/covoituragemobile/ManageUser.java
@@ -0,0 +1,15 @@
+package uqtr.covoituragemobile;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ManageUser extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_manage_user);
+		
+		
+	}
+}
